ecafdae54ad84d24516e1a899ee7188541f3fd36
jaybhagat <jaytrupti@gmail.com>
2021-05-11 22:46:00 -0400

    Finishing the front-end

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
-import Header from './components/Header';
+import Header from './components/Header.js';
 import Feed from './components/Feed'
-import Profile from './components/Profile'
-import ArticleView from './components/ArticleView'
-import Editor from './components/Editor'
+import Profile from './components/Profile.js'
+import ArticleView from './components/ArticleView.js'
+import Editor from './components/Editor.js'
 import requireAuthentication from './utils/requireAuth'
-import SignInWith from './components/SignInWith'
+import SignInWith from './components/SignInWith.js'
 class App extends Component {
     render() {
         const pathname = window.location.pathname
--- src/index.js
+++ src/index.js
@@ -7,16 +7,26 @@ import { Switch, Route } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
 import reportWebVitals from './reportWebVitals';
 import { store, history } from './redux/store'
+import { getUser } from './redux/actions/actions'
 
-ReactDOM.render(
-  <Provider store={store}>
+if(localStorage.Auth) {
+    // update localstorage
+    store.dispatch({type: 'SET_USER', user: JSON.parse(localStorage.Auth)})
+    var _id = JSON.parse(localStorage.Auth)._id
+    getUser(_id).then((res) => {
+        store.dispatch({type: 'SET_USER', user: res})
+    })
+}
+
+ReactDOM.render((
+    <Provider store={store}>
         <ConnectedRouter history={history}>
             <Switch>
                 <Route path="/" component={App} />
             </Switch>
         </ConnectedRouter>
-    </Provider>,
-  document.getElementById('root')
+    </Provider>
+  ), document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
